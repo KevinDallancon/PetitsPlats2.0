6b616f385681fb680cf7e7a7c102e84e140055d1
Kévin Dallançon <kevin.dallancon@hotmail.fr>
2024-08-22 12:27:50 +0200

    add message 0 recette

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -4,11 +4,20 @@ function displayData(recettes) {
   const recetteSection = document.querySelector(".recettes_section");
   recetteSection.innerHTML = "";
 
-  recettes.forEach((recette) => {
-    const recetteModel = recipeTemplate(recette);
-    const userCardDOM = recetteModel.getCardDOM();
-    recetteSection.appendChild(userCardDOM);
-  });
+  if (recettes.length === 0) {
+    const searchInput = document.querySelector(".search-input");
+    const searchTerm = searchInput.value;
+    const noResultMessage = document.createElement("p");
+    noResultMessage.classList.add("no-result-message");
+    noResultMessage.textContent = `Aucune recette ne contient '${searchTerm}'. Vous pouvez chercher « tarte aux pommes », « poisson », etc.`;
+    recetteSection.appendChild(noResultMessage);
+  } else {
+    recettes.forEach((recette) => {
+      const recetteModel = recipeTemplate(recette);
+      const userCardDOM = recetteModel.getCardDOM();
+      recetteSection.appendChild(userCardDOM);
+    });
+  }
   updateTotal(recettes.length);
 }
 
